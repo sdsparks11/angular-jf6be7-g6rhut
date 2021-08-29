d045b21e8219c4a0dcc791d077884430e89eda7d
sdsparks11 <suberthi1998@gmail.com>
2021-08-29 20:30:37 +0530

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertComponent } from './product-alert/product-alert.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   imports: [
@@ -16,7 +17,8 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
-      {path:'products/:productId', component:ProductDetailsComponent}
+      {path:'products/:productId', component:ProductDetailsComponent},
+      { path: 'cart', component: CartComponent }
     ])
   ],
   declarations: [
